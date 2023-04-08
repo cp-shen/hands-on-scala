4e527e8491a4b9ef279ffa6239c72d8862eb06f6
scp <scp@archlinuxscp.xyz>
2023-04-08 20:19:11 +0800

    add Example.test.scala, Project.scala

---
+++ Example.test.scala
@@ -0,0 +1,7 @@
+
+class MyTests extends munit.FunSuite {
+  test("test") {
+    val x = 2
+    assertEquals(x, 2)
+  }
+}
---
+++ Project.scala
@@ -0,0 +1,5 @@
+//> using scala "2.13"
+//> using jvm "system"
+//> using dep "com.lihaoyi::os-lib::0.9.1"
+//> using dep "org.scalameta::munit::1.0.0-M7"
+
